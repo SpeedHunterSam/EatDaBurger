076d0aee6cf8f7a00ce56d7b7dc5482b97641717
Brian <brian.s2@gmail.com>
2019-09-26 23:44:41 -0500

    basic orm and 1st route created

--- config/orm.js
+++ config/orm.js
@@ -1,23 +1,41 @@
 const connection = require("../config/connection.js");
 
+// Object Relational Mapper (ORM)
 
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+var orm = {
 
+    selectAll: function (tableInput){
+        const queryString = `SELECT * FROM ??`;
+        connection.query(queryString, [tableInput], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+          });
 
-/* 
+    },
+    insertOne: function (tableInput){
+        const queryString = `SELECT * FROM ??`;
+        connection.query(queryString, [tableInput], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+          });
 
+    },
+    updateOne: function (tableInput){
+        const queryString = `SELECT * FROM ??`;
+        connection.query(queryString, [tableInput], function(err, result) {
+            if (err) throw err;
+            console.log(result);
+          });
 
-Sequelize code here
-
-
-
-*/
-
-
-
-
-
-//module.exports = orm;
-
+    }
+  };
+  
+  module.exports = orm;
+  
 /*
 * `selectAll()`
 * `insertOne()`
--- models/burger.js
+++ models/burger.js
@@ -1,14 +1,30 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
-/* 
+const burger = {
+    all: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res) {
+        cb(res);
+      });
+    },
+    update: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res) {
+        cb(res);
+      });
+    },
+    delete: function(condition, cb) {
+      orm.delete("burgers", condition, function(res) {
+        cb(res);
+      });
+    }
+  };
 
 
-Sequelize code here
-
-
-
-*/
-
-// Export the database functions for the controller (catsController.js).
-//module.exports = burger;
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
